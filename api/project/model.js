467dd11655b4ce3964c6b8c382d6d5bceb1f9f3d
Ryan <snorekr@gmail.com>
2021-12-10 12:09:08 -0500

    fixed GET projects, boolean conversion

--- api/project/model.js
+++ api/project/model.js
@@ -1,11 +1,23 @@
 const db = require('../../data/dbConfig');
 
-const getProjects = () => db('projects');
+const boolifyNum = (num) => num === 1 ? true : false;
+
+const getProjects = async () => {
+    const projects = await db('projects');
+    return projects.map(p => {
+        const num = p.project_completed;
+        return ({
+            ...p,
+            project_completed: boolifyNum(num)
+        })
+    })
+};
 
 const addProject = (newProject) => {
     return db('projects')
         .insert(newProject)
         .then(([project_id]) => {
+
             return db('projects')
                 .where('project_id', project_id)
                 .first()
