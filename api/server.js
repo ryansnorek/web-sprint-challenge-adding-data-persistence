ef7b82c2d6d84b15c5bf396a415f8810cdfa2e7a
Ryan <snorekr@gmail.com>
2021-12-10 08:55:21 -0500

    first commit, server setup

--- api/server.js
+++ api/server.js
@@ -1,1 +1,14 @@
-// build your server here and require it from index.js
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+server.use((e, req, res, next) => { //eslint-disable-line
+    res.status(500).json({
+        message: e.message,
+        stack: e.stack
+    })
+})
+
+module.exports = server;
